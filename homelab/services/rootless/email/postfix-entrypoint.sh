465158729cfcb792b98af628eab87c82cee8f9b6
Ebrahim <53321702+ebrahim37@users.noreply.github.com>
2026-09-04 14:54:50 -0400

    remove snappymail and add new email stack, also aio listen on all interfaces

---
+++ homelab/services/rootless/email/postfix-entrypoint.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+set -eu
+
+if [ ! -d /var/spool/postfix/maildrop ]; then
+	cp -a /var/spool/postfix.default/. /var/spool/postfix/
+fi
+
+postconf -e 'compatibility_level = 3.6'
+postconf -e 'myhostname = email.local'
+postconf -e 'inet_interfaces = all'
+postconf -e 'inet_protocols = ipv4'
+postconf -e 'mynetworks = 127.0.0.0/8, 10.0.2.0/24, 169.254.0.0/16'
+postconf -e 'smtpd_relay_restrictions = permit_mynetworks, reject'
+postconf -e 'smtpd_sender_restrictions = check_sender_access hash:/etc/postfix/allowed_senders, reject'
+postconf -e 'relayhost = [smtp.gmail.com]:587'
+postconf -e 'sender_dependent_relayhost_maps = hash:/etc/postfix/sender_relay'
+postconf -e 'smtp_sender_dependent_authentication = yes'
+postconf -e 'smtp_sasl_auth_enable = yes'
+postconf -e 'smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd'
+postconf -e 'smtp_sasl_security_options = noanonymous'
+postconf -e 'smtp_sasl_tls_security_options = noanonymous'
+postconf -e 'smtp_sasl_mechanism_filter = plain, login'
+postconf -e 'smtp_tls_security_level = encrypt'
+postconf -e 'smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt'
+postconf -e 'maillog_file = /dev/stdout'
+
+cat > /etc/postfix/sender_relay <<'EOF'
+{{ homelab.priv_main_email }} [smtp.gmail.com]:587
+hireme@ebra.dev [smtp.gmail.com]:587
+me@ebra.dev [smtp.gmail.com]:587
+sayhi@ebra.dev [smtp.gmail.com]:587
+{{ homelab.priv_qa_email }} [smtp.gmail.com]:587
+auth@ebra.dev [smtp.gmail.com]:587
+vaultwarden@ebra.dev [smtp.gmail.com]:587
+{{ homelab.priv_icloud_email }} [smtp.mail.me.com]:587
+EOF
+
+cat > /etc/postfix/allowed_senders <<'EOF'
+{{ homelab.priv_main_email }} OK
+hireme@ebra.dev OK
+me@ebra.dev OK
+sayhi@ebra.dev OK
+{{ homelab.priv_qa_email }} OK
+auth@ebra.dev OK
+vaultwarden@ebra.dev OK
+{{ homelab.priv_icloud_email }} OK
+EOF
+
+umask 077
+{
+	printf '%s %s:%s\n' '{{ homelab.priv_main_email }}' '{{ homelab.priv_main_email }}' "$GMAIL_MAIN_APP_PASSWORD"
+	printf '%s %s:%s\n' 'hireme@ebra.dev' '{{ homelab.priv_main_email }}' "$GMAIL_MAIN_APP_PASSWORD"
+	printf '%s %s:%s\n' 'me@ebra.dev' '{{ homelab.priv_main_email }}' "$GMAIL_MAIN_APP_PASSWORD"
+	printf '%s %s:%s\n' 'sayhi@ebra.dev' '{{ homelab.priv_main_email }}' "$GMAIL_MAIN_APP_PASSWORD"
+	printf '%s %s:%s\n' '{{ homelab.priv_qa_email }}' '{{ homelab.priv_qa_email }}' "$GMAIL_QA_APP_PASSWORD"
+	printf '%s %s:%s\n' 'auth@ebra.dev' '{{ homelab.priv_qa_email }}' "$GMAIL_QA_APP_PASSWORD"
+	printf '%s %s:%s\n' 'vaultwarden@ebra.dev' '{{ homelab.priv_qa_email }}' "$GMAIL_QA_APP_PASSWORD"
+	printf '%s %s:%s\n' '{{ homelab.priv_icloud_email }}' '{{ homelab.priv_icloud_email }}' "$ICLOUD_APP_PASSWORD"
+	printf '%s %s:%s\n' '[smtp.gmail.com]:587' '{{ homelab.priv_main_email }}' "$GMAIL_MAIN_APP_PASSWORD"
+	printf '%s %s:%s\n' '[smtp.mail.me.com]:587' '{{ homelab.priv_icloud_email }}' "$ICLOUD_APP_PASSWORD"
+} > /etc/postfix/sasl_passwd
+
+postmap hash:/etc/postfix/sender_relay
+postmap hash:/etc/postfix/allowed_senders
+postmap hash:/etc/postfix/sasl_passwd
+postfix set-permissions
+postfix check
+
+exec postfix start-fg
---
+++ homelab/services/rootless/email/roundcube-config.php
@@ -0,0 +1,10 @@
+<?php
+
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['identities_level'] = 0;
+$config['create_default_folders'] = false;
+$config['drafts_mbox'] = null;
+$config['junk_mbox'] = null;
+$config['sent_mbox'] = null;
+$config['trash_mbox'] = null;
