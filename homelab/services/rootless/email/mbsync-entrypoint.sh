46bd8e1bab803b244ee10d8c639128f3629ef61d
Ebrahim <53321702+ebrahim37@users.noreply.github.com>
2026-09-04 15:08:51 -0400

    fix maildir

--- homelab/services/rootless/email/mbsync-entrypoint.sh
+++ homelab/services/rootless/email/mbsync-entrypoint.sh
@@ -3,6 +3,10 @@ set -u
 
 trap 'exit 0' INT TERM
 
+for mailbox in "gmail main" "gmail qa" icloud; do
+	/bin/mkdir -p "/mail/$mailbox/cur" "/mail/$mailbox/new" "/mail/$mailbox/tmp"
+done
+
 while :; do
 	/usr/bin/mbsync --config /etc/mbsyncrc --all ||
 		echo "mbsync failed; retrying in three minutes" >&2
@@ -10,4 +14,3 @@ while :; do
 	/bin/sleep 180 &
 	wait $! || exit 0
 done
-
