aa228dcffa92a736b5fcbb4c5171c7dab8d3d542
Ebrahim <53321702+ebrahim37@users.noreply.github.com>
2026-09-14 04:19:02 -0400

    fix roundcube timezone

--- homelab/services/rootless/email/roundcube-config.php
+++ homelab/services/rootless/email/roundcube-config.php
@@ -2,6 +2,8 @@
 
 $config['smtp_user'] = '';
 $config['smtp_pass'] = '';
+$config['timezone'] = 'America/Toronto';
+$config['dont_override'][] = 'timezone';
 $config['drafts_mbox'] = null;
 $config['junk_mbox'] = null;
 $config['sent_mbox'] = null;
